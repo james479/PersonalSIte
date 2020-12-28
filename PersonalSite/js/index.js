aeead6d1717579a86f98bf4649cd3a9f43f089f1
James Glover <james.e.glover1@gmail.com>
2020-12-27 22:23:07 -0600

    Finished home page setup

---
+++ PersonalSite/js/index.js
@@ -0,0 +1,7 @@
+﻿const mainNav = document.getElementById('js-menu');
+
+const navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+});
